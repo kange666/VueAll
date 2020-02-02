44ac5d0ec45632808691b13b95798696821419dc
liuzishikang <1142946562@qq.com>
2020-02-02 14:36:33 +0800

    初始化

---
+++ LearnVuejs06/learnvuerouter/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h2>我是APP组件</h2>
+    <!--<router-link to="/home" tag="button" replace active-class="active">首页</router-link>-->
+    <!--<router-link to="/about" tag="button" replace active-class="active">关于</router-link>-->
+    <!--<router-link to="/home" tag="button" replace>首页</router-link>-->
+    <!--<router-link to="/about" tag="button" replace>关于</router-link>-->
+    <button @click="homeClick">首页</button>
+    <button @click="aboutClick">关于</button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    homeClick() {
+      // 通过代码的方式修改路由 vue-router
+      // push => pushState
+      // this.$router.push('/home')
+      this.$router.replace('/home')
+      console.log('homeClick');
+    },
+    aboutClick() {
+      // this.$router.push('/about')
+      this.$router.replace('/about')
+      console.log('aboutClick');
+    }
+  }
+}
+</script>
+
+<style>
+  /*.router-link-active {*/
+    /*color: #f00;*/
+  /*}*/
+
+  .active {
+    color: #f00;
+  }
+</style>
---
+++ LearnVuejs06/learnvuerouter/src/router/index.js
@@ -0,0 +1,36 @@
+// 配置路由相关的信息
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+import Home from '../components/Home'
+import About from '../components/About'
+
+// 1.通过Vue.use(插件), 安装插件
+Vue.use(VueRouter)
+
+// 2.创建VueRouter对象
+const routes = [
+  {
+    path: '',
+    // redirect重定向
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/about',
+    component: About
+  }
+]
+const router = new VueRouter({
+  // 配置路由和组件之间的应用关系
+  routes,
+  mode: 'history',
+  linkActiveClass: 'active'
+})
+
+// 3.将router对象传入到Vue实例
+export default router
+
