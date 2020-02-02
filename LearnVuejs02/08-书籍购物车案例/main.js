b445e8d5fe4df2eb36b8adc24031c157ed50a8da
liuzishikang <1142946562@qq.com>
2020-02-02 14:12:04 +0800

    初始化

---
+++ LearnVuejs02/08-书籍购物车案例/main.js
@@ -0,0 +1,66 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    books: [
+      {
+        id: 1,
+        name: '《算法导论》',
+        date: '2006-9',
+        price: 85.00,
+        count: 1
+      },
+      {
+        id: 2,
+        name: '《UNIX编程艺术》',
+        date: '2006-2',
+        price: 59.00,
+        count: 1
+      },
+      {
+        id: 3,
+        name: '《编程珠玑》',
+        date: '2008-10',
+        price: 39.00,
+        count: 1
+      },
+      {
+        id: 4,
+        name: '《代码大全》',
+        date: '2006-3',
+        price: 128.00,
+        count: 1
+      },
+    ]
+  },
+  methods: {
+    // getFinalPrice(price) {
+    //   return '¥' + price.toFixed(2)
+    // }
+    increment(index) {
+      this.books[index].count++
+    },
+    decrement(index) {
+      this.books[index].count--
+    },
+    removeHandle(index) {
+      this.books.splice(index, 1)
+    }
+  },
+  computed: {
+    totalPrice() {
+      let totalPrice = 0
+      for (let i = 0; i < this.books.length; i++) {
+        totalPrice += this.books[i].price * this.books[i].count
+      }
+      return totalPrice
+
+      // for (let i in/of this.books)
+      // reduce
+    }
+  },
+  filters: {
+    showPrice(price) {
+      return '¥' + price.toFixed(2)
+    }
+  }
+})
